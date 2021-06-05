8ade1c9f199d5de6c905e1593b6524d97dd59013
EC2 Default User <ec2-user@ip-172-31-15-181.ec2.internal>
2021-06-05 19:08:40 +0000

    Controlador Home preparado

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def landing
+  end
+
+  def about_us
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#landing'
+  get 'about_us' => 'home#about_us' 
+  get 'contact' => 'home#contact'
   get 'legal/privacy'
   get 'legal/terms_of_use' => 'legal#terms' 
   get 'legal/cookies'
